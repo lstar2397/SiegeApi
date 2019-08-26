8e8ce556581e67e5afdb949f7d66ae4aafe26d2d
Starlet <lstar2397@naver.com>
2019-08-26 22:04:56 +0900

    Add sample project

---
+++ SiegeApiSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SiegeApi;
+using SiegeApi.Enums;
+
+namespace SiegeApiSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Api api = Api.GetInstance(email: "Your ubisoft email", password: "Your ubisoft password");
+
+            var player = api.GetPlayerByName("etco", Platform.PLAYSTATION).Result;
+            Console.WriteLine($"player.ProfileId: {player.ProfileId}");
+            Console.WriteLine($"player.UserId: {player.UserId}");
+            Console.WriteLine($"player.PlatformType: {player.PlatformType}");
+            Console.WriteLine($"player.IdOnPlatform: {player.IdOnPlatform}");
+            Console.WriteLine($"player.NameOnPlatform: {player.NameOnPlatform}");
+        }
+    }
+}
