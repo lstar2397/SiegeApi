72723d9b311757d435d6390af103bdbdc0cde142
Starlet <lstar2397@naver.com>
2019-08-27 14:38:50 +0900

    Add UriExtensions class

---
+++ SiegeApi/Extensions/UriExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+
+namespace SiegeApi.Extensions
+{
+    public static class UriExtensions
+    {
+        public static Uri AddParameter(this Uri uri, string name, string value)
+        {
+            UriBuilder uriBuilder = new UriBuilder(uri);
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            query[name] = value;
+            uriBuilder.Query = query.ToString();
+
+            return uriBuilder.Uri;
+        }
+    }
+}
