ebd3086a5b9387b6370f7d75c9eb4906b661d543
Starlet <lstar2397@naver.com>
2019-08-28 10:02:44 +0900

    Add progressionUrls

--- SiegeApi/Constants.cs
+++ SiegeApi/Constants.cs
@@ -15,5 +15,12 @@ namespace SiegeApi
             { Platform.Xbox, "https://public-ubiservices.ubi.com/v1/spaces/05bfb3f7-6c21-4c42-be1f-97a33fb5cf66/sandboxes/OSBOR_XBOXONE_LNCH_A/r6karma/players" },
             { Platform.Playstation, "https://public-ubiservices.ubi.com/v1/spaces/98a601e5-ca91-4440-b1c5-753f601a2c90/sandboxes/OSBOR_PS4_LNCH_A/r6karma/players" }
         };
+
+        public static readonly Dictionary<Platform, string> ProgressionUrls = new Dictionary<Platform, string>()
+        {
+            { Platform.Uplay, "https://public-ubiservices.ubi.com/v1/spaces/5172a557-50b5-4665-b7db-e3f2e8c5041d/sandboxes/OSBOR_PC_LNCH_A/r6playerprofile/playerprofile/progressions" },
+            { Platform.Xbox, "https://public-ubiservices.ubi.com/v1/spaces/05bfb3f7-6c21-4c42-be1f-97a33fb5cf66/sandboxes/OSBOR_XBOXONE_LNCH_A/r6playerprofile/playerprofile/progressions" },
+            { Platform.Playstation, "https://public-ubiservices.ubi.com/v1/spaces/98a601e5-ca91-4440-b1c5-753f601a2c90/sandboxes/OSBOR_PS4_LNCH_A/r6playerprofile/playerprofile/progressions" }
+        };
     }
 }
